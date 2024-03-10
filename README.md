c23d2a623c6a2998a6b1be66d2789a0c1ec7d8bc
kaung min khant <kaungminkhant.official@gmail.como>
2024-03-10 16:55:21 +0630

    modified readme

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## Description
 
-## Getting Started
+This project take a very shallow dip to the world of ThreeJS with NextJS using React Three Fiber (R3F) library. Not very impressive stuff, but the basics and some concepts. This project may be updated gradually, as I learn more about ThreeJS in the future. I may do a seperate or use this project to learn more about vanilla ThreeJS as well.
 
-First, run the development server:
+## To run
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+1. `npm install`
+2. `npm run dev` for development
+3. `npm run build & npm run start` for production
--- src/components/threeSamples/Sample1.tsx
+++ src/components/threeSamples/Sample1.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { MeshWobbleMaterial, OrbitControls, useHelper } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Leva, useControls } from "leva";
 import { useRef, useState } from "react";
 import { DirectionalLight, DirectionalLightHelper, Mesh } from "three";
 
@@ -15,7 +16,7 @@ interface BoxProps extends GeometryProps {
 const Box = ({
   position = [0, 0, 0],
   size = [1, 1, 1],
-  color = "red",
+  color,
   wobble = false,
 }: BoxProps) => {
   const [isClicked, setIsClicked] = useState(false);
@@ -28,7 +29,7 @@ const Box = ({
   });
   let material;
   if (wobble) {
-    material = <MeshWobbleMaterial color={color} factor={3} speed={5} />;
+    material = <MeshWobbleMaterial color={color} factor={3} speed={3} />;
   } else {
     material = <meshStandardMaterial color={color} />;
   }
@@ -74,26 +75,46 @@ const Scene = () => {
   const directionalLightRef = useRef<DirectionalLight>(null!);
 
   useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "red");
+
+  const {lightColor, lightIntensity} = useControls({
+    lightColor: "red",
+    lightIntensity: {
+      value: 0.5,
+      min: 0,
+      max: 5,
+      step: 0.1,
+    },
+  });
+
+  const {radius} = useControls({
+    radius: {
+      value: 1,
+      min: 1,
+      max: 4,
+      step: 0.1
+    }
+  })
   return (
     <>
-      <directionalLight position={[0, 0, 5]} ref={directionalLightRef} />
+      <directionalLight position={[0, 0, 5]} ref={directionalLightRef} color={lightColor} intensity={lightIntensity} />
       <directionalLight position={[0, 0, -5]} />
-      <Box size={[1, 1, 1]} position={[-2, 0, 0]} color="red" wobble />
-      <Box size={[1, 1, 1]} position={[2, 0, 0]} color="red" />
+      <Box size={[1, 1, 1]} position={[-2, 0, 0]} wobble />
+      <Box size={[1, 1, 1]} position={[2, 0, 0]}/>
       <Sphere
-        size={[0.8, 32, 32, 0, Math.PI * 2, 0, Math.PI * 2]}
+        size={[radius, 32, 32, 0, Math.PI * 2, 0, Math.PI * 2]}
         position={[0, -2, 0]}
         color="red"
       />
       <Box size={[1, 1, 1]} position={[0, 2, 0]} color="red" />
 
-      <OrbitControls autoRotate enableZoom={false} />
+      <OrbitControls enableZoom={false} />
     </>
   );
 };
 export default function Sample1() {
   return (
     <div className="flex flex-col h-screen w-full">
+      <Leva />
       <p className="text-center my-5">Sample 1 Canvas</p>
       <div className="flex-grow m-5 mt-0">
         <Canvas className="border border-foreground">
